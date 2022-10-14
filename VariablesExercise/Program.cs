0dee14b97ba461548a0f8c17f000d6a25d9010e8
yahel dooley <ydooley@gmail.com>
2022-10-14 16:43:29 -0700

    declared and initialized variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Timmy";
+            int age = 14;
+            char thisChar = '@';
+            bool hasTimmyFallenDownAWell = true;
+            double treasureValue = 18000000;
+            decimal afterTaxValue = 900m;
+
+            name = (name == null || name == "") ? "unknown" : name;
+
+            Console.WriteLine($"A {age} year old kid named {name}, fell down a well, but survived!\n");
+            Console.WriteLine($"Luckily he landed on a pile of cushy gold coins and other pirate's booty worth ${treasureValue}.\n");
+            Console.WriteLine($"Well...${afterTaxValue} after taxes anyway...");
         }
     }
 }
